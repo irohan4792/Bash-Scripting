31648711f5a1f352a8ab9c398ebbadccb806ff04
RohanSharma4792 <rohan.sharma2020@vitbhopal.ac.in>
2024-03-10 11:02:12 +0530

    basic scripts

---
+++ first_script.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+echo "hello this is your first argument ${1}"
+#echo "my first shell script"
+name="rohan"
+echo "hello ${name}, please enter your age"
+read age 
+sleep 2
+echo "my age is ${age}"
---
+++ if_script.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+if [ "$1" = "bacha" ]
+then 
+ echo "you are under age"
+else 
+ echo "your age is perfect:)"
+fi 
+
+
